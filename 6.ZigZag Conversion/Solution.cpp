05370bbc967ec6db03a9e91f3cfd6dcf60c3ed46
Jianshu Yang <283131709@qq.com>
2020-07-04 16:16:29 +0800

    Update Solution.cpp

--- 6.ZigZag Conversion/Solution.cpp
+++ 6.ZigZag Conversion/Solution.cpp
@@ -4,6 +4,7 @@ public:
         if(numRows <= 1)
             return s;
 
+        #行间隔公式 2*numRows - 2
         int size = 2*numRows - 2;
         int n = s.size();
         string res;
@@ -11,6 +12,7 @@ public:
         for(int i = 0; i < numRows; ++i){
             for(int j = i; j < n; j+=size){
                 res += s[j];
+                #之字中间值公式 j + 2*numRows-2 - 2*i
                 int pos = j + size - 2*i;
                 if(i != 0 && i != numRows - 1 && pos < n)
                     res += s[pos];
